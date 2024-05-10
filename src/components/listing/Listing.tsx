bbb89fb455d6799042642b4fe3ad3e3478fd72d1
darnisha29 <darnisha2910@gmail.com>
2024-05-10 10:32:40 +0530

    some ui changes

--- src/components/listing/Listing.tsx
+++ src/components/listing/Listing.tsx
@@ -3,52 +3,45 @@ import './listing.css'
 import axios from 'axios';
 
 interface ChildComponentProps {
-  tableName: any; 
-  setTableName: any; 
-  TABLE:any
+  tableName: any;
+  setTableName: any;
+  TABLE: any;
 }
 
+const Listing: React.FC<ChildComponentProps> = ({ tableName, setTableName, TABLE }) => {
+  const [tables, setTables] = useState<string[]>([]);
+  const [selectedTable, setSelectedTable] = useState<string>('employee_data');
 
-const Listing: React.FC<ChildComponentProps> = ({tableName,setTableName,TABLE}) => {
-    // const tables = ["users","company",'profile']
-    const [tables ,setTables] = useState([])
-    
-    useEffect(() => {
-    
-      axios
-        .get('http://localhost:8000/api/tableNames') 
-        .then((response) => {
-          console.log("here is the response .......... in listing",response.data.data);
-          setTables(response.data.data)
-      
-        })
-        .catch((error) => {
-          console.error('Error fetching metadata:', error);
-        });
-    }, []);
-    useEffect(() => {
-      console.log(tableName);
-    },[setTableName]) 
-    function clickHandler(table:string): void {
-      // console.log(table);
-      setTableName(table);
-      
-      TABLE=table;
-      console.log(TABLE);
-    }
+  useEffect(() => {
+    axios
+      .get('http://localhost:8000/api/tableNames')
+      .then((response) => {
+        console.log("here is the response .......... in listing", response.data.data);
+        setTables(response.data.data);
+      })
+      .catch((error) => {
+        console.error('Error fetching metadata:', error);
+      });
+  }, []);
+
+  function clickHandler(table: string): void {
+    console.log("table", table);
+    setTableName(table);
+    setSelectedTable(table);
+  }
+
   return (
     <div className='tables-container'>
-        <h2>Tables</h2>
-      {tables?tables.map((table:string,index:any) => {
-        
-
-        return(
-            <p key={index} onClick={() => clickHandler(table)}>{table}</p>
-        )
+      <h2>Tables</h2>
+      {tables ? tables.map((table: string, index: number) => {
+        const isSelected = selectedTable === table;
+        return (
+          <p key={index} className={isSelected ? 'selected-table' : ''} onClick={() => clickHandler(table)}>{table}</p>
+        );
       })
-      : (null)}
+        : (null)}
     </div>
-  )
+  );
 }
 
-export default Listing
+export default Listing;
